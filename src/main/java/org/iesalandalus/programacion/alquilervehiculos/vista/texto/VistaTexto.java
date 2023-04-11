611d4296e0f230801ca977b44c66040ab6b2b22c
Ismail <Ismail@DESKTOP-ED6PBNR>
2023-04-11 11:11:43 +0200

    El proyecto pasa todos los tests correctamente.

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/vista/texto/VistaTexto.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/vista/texto/VistaTexto.java
@@ -8,7 +8,6 @@ package org.iesalandalus.programacion.alquilervehiculos.vista.texto;
 import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.EnumMap;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
